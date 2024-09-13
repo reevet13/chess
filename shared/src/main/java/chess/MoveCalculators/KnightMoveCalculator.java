fb0e7ec1d49462408df8cf0b11fc00bf6acc54c2
Tanner Reeve <reevet@byu.edu>
2024-09-13 14:06:30 -0600

    knight passed all tests

--- shared/src/main/java/chess/MoveCalculators/KnightMoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculators/KnightMoveCalculator.java
@@ -15,42 +15,58 @@ public class KnightMoveCalculator {
         //up up right
         if (startPosition.getRow() <= 6 && startPosition.getColumn() <= 7) {
             endPosition = new ChessPosition(startPosition.getRow() + 2, startPosition.getColumn() + 1);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //up right right
         if (startPosition.getRow() <= 7 && startPosition.getColumn() <= 6) {
             endPosition = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() + 2);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //down right right
         if (startPosition.getRow() >= 2 && startPosition.getColumn() <= 6) {
             endPosition = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() + 2);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //down down right
         if (startPosition.getRow() >= 3 && startPosition.getColumn() <= 7) {
             endPosition = new ChessPosition(startPosition.getRow() - 2, startPosition.getColumn() + 1);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //down down left
         if (startPosition.getRow() >= 3 && startPosition.getColumn() >= 2) {
             endPosition = new ChessPosition(startPosition.getRow() - 2, startPosition.getColumn() - 1);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //down left left
         if (startPosition.getRow() >= 2 && startPosition.getColumn() >= 3) {
             endPosition = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() - 2);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //up left left
         if (startPosition.getRow() <= 7 && startPosition.getColumn() >= 3) {
             endPosition = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() - 2);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         //up up left
         if (startPosition.getRow() <= 6 && startPosition.getColumn() >= 2) {
             endPosition = new ChessPosition(startPosition.getRow() + 2, startPosition.getColumn() - 1);
-            knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
+                knightMoves.add(new ChessMove(startPosition, endPosition, null));
+            }
         }
         return knightMoves;
     }
