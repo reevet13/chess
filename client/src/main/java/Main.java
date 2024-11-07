579665e2c03d6db2ee06e9139a01cfabb5253d26
Tanner Reeve <reevet@byu.edu>
2024-11-07 10:45:41 -0700

    main updated

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import chess.*;
 import client.ServerFacade;
+import ui.PreloginREPL;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -7,5 +8,9 @@ public class Main {
         System.out.println("♕ 240 Chess Client: " + piece);
 
         ServerFacade server = new ServerFacade();
+
+        PreloginREPL pre_login = new PreloginREPL(server);
+        pre_login.run();
+        System.out.println("Exited");
     }
 }
