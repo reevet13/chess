aa17bdc1f460894d2f973ada2fd8a31405251f47
Tanner Reeve <reevet@byu.edu>
2024-11-11 17:18:51 -0700

    main update

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,21 +1,14 @@
 import chess.*;
 import client.ServerFacade;
-import server.Server;
 import ui.PreloginREPL;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client:");
 
-        Server server = new Server();
-        int port = server.run(0); // 0 means to use any available port
-        System.out.println("Started server on port: " + port);
+        ServerFacade server = new ServerFacade();
 
-        // Initialize ServerFacade with the dynamic port
-        ServerFacade serverFacade = new ServerFacade("http://localhost:" + port);
-
-        PreloginREPL prelogin = new PreloginREPL(serverFacade);
+        PreloginREPL prelogin = new PreloginREPL(server);
         prelogin.run();
         System.out.println("Exited");
 
