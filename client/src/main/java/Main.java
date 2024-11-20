2fc32b059e943c2f7acaf6795ef86c4350d21275
Tanner Reeve <reevet@byu.edu>
2024-11-20 13:32:52 -0700

    main updated

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,7 +3,7 @@ import client.ServerFacade;
 import ui.PreloginREPL;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
 
