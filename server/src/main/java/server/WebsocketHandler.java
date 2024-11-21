8edad0ea63aad8f042b2eac583270f33046fc7c3
Tanner Reeve <reevet@byu.edu>
2024-11-21 13:56:09 -0700

    websocket handler updated

--- server/src/main/java/server/WebsocketHandler.java
+++ server/src/main/java/server/WebsocketHandler.java
@@ -7,18 +7,180 @@ import dataaccess.BadRequestException;
 import dataaccess.UnauthorizedException;
 import model.AuthData;
 import model.GameData;
+import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
-import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.serverMessages.Error;
+import webSocketMessages.serverMessages.LoadGame;
+import webSocketMessages.serverMessages.Notification;
+import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.*;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @WebSocket
 public class WebsocketHandler {
 
+    @OnWebSocketConnect
+    public void onConnect(Session session) throws Exception {
+        Server.gameSessions.put(session, 0);
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {
+        Server.gameSessions.remove(session);
+    }
+
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
-        System.out.printf("Received: %s", message);
-        session.getRemote().sendString("WebSocket response: " + message);
+        System.out.printf("Received: %s\n", message);
+
+        if (message.contains("\"commandType\":\"JOIN_PLAYER\"")) {
+            JoinPlayer command = new Gson().fromJson(message, JoinPlayer.class);
+            Server.gameSessions.replace(session, command.getGameID());
+            handleJoinPlayer(session, command);
+        }
+        else if (message.contains("\"commandType\":\"JOIN_OBSERVER\"")) {
+            JoinObserver command = new Gson().fromJson(message, JoinObserver.class);
+            Server.gameSessions.replace(session, command.getGameID());
+            handleJoinObserver(session, command);
+        }
+        else if (message.contains("\"commandType\":\"MAKE_MOVE\"")) {
+            MakeMove command = new Gson().fromJson(message, MakeMove.class);
+            handleMakeMove(session, command);
+        }
+        else if (message.contains("\"commandType\":\"LEAVE\"")) {
+            Leave command = new Gson().fromJson(message, Leave.class);
+            handleLeave(session, command);
+        }
+        else if (message.contains("\"commandType\":\"RESIGN\"")) {
+            Resign command = new Gson().fromJson(message, Resign.class);
+            handleResign(session, command);
+        }
     }
+
+    private void handleJoinPlayer(Session session, JoinPlayer command) throws IOException {
+
+        try {
+            AuthData auth = Server.userService.getAuth(command.getAuthString());
+            Notification notif = new Notification("%s has joined the game as %s".formatted(auth.username(), command.getColorString()));
+            broadcastMessage(session, notif);
+        }
+        catch (UnauthorizedException e) {
+            sendError(session, new Error("Error: Not authorized"));
+        }
+
+    }
+
+    private void handleJoinObserver(Session session, JoinObserver command) throws IOException {
+        try {
+            AuthData auth = Server.userService.getAuth(command.getAuthString());
+            Notification notif = new Notification("%s has joined the game as an observer".formatted(auth.username()));
+            broadcastMessage(session, notif);
+        }
+        catch (UnauthorizedException e) {
+            sendError(session, new Error("Error: Not authorized"));
+        }
+    }
+
+    private void handleMakeMove(Session session, MakeMove command) throws IOException {
+        try {
+            AuthData auth = Server.userService.getAuth(command.getAuthString());
+            GameData game = Server.gameService.getGameData(command.getAuthString(), command.getGameID());
+            ChessGame.TeamColor userColor = getTeamColor(auth.username(), game);
+            if (userColor == null) {
+                sendError(session, new Error("Error: You are observing this game"));
+                return;
+            }
+
+            if (game.game().getTeamTurn().equals(userColor)) {
+                game.game().makeMove(command.getMove());
+                Server.gameService.updateGame(auth.authToken(), game);
+                LoadGame load = new LoadGame(game.game());
+                broadcastMessage(session, load, true);
+            }
+            else {
+                sendError(session, new Error("Error: it is not your turn"));
+            }
+        }
+        catch (UnauthorizedException e) {
+            sendError(session, new Error("Error: Not authorized"));
+        } catch (BadRequestException e) {
+            sendError(session, new Error("Error: invalid game"));
+        } catch (InvalidMoveException e) {
+            sendError(session, new Error("Error: invalid move"));
+        }
+    }
+
+    private void handleLeave(Session session, Leave command) throws IOException {
+        try {
+            AuthData auth = Server.userService.getAuth(command.getAuthString());
+
+            Notification notif = new Notification("%s has left the game".formatted(auth.username()));
+            broadcastMessage(session, notif);
+
+            session.close();
+        } catch (UnauthorizedException e) {
+            sendError(session, new Error("Error: Not authorized"));
+        }
+    }
+
+    private void handleResign(Session session, Resign command) throws IOException {
+        try {
+            AuthData auth = Server.userService.getAuth(command.getAuthString());
+            GameData game = Server.gameService.getGameData(command.getAuthString(), command.getGameID());
+            ChessGame.TeamColor userColor = getTeamColor(auth.username(), game);
+
+            String opponentUsername = userColor == ChessGame.TeamColor.WHITE ? game.blackUsername() : game.whiteUsername();
+
+            if (userColor == null) {
+                sendError(session, new Error("Error: You are observing this game"));
+                return;
+            }
+
+            Notification notif = new Notification("%s has forfeited, %s wins!".formatted(auth.username(), opponentUsername));
+            broadcastMessage(session, notif, true);
+        } catch (UnauthorizedException e) {
+            sendError(session, new Error("Error: Not authorized"));
+        } catch (BadRequestException e) {
+            sendError(session, new Error("Error: invalid game"));
+        }
+    }
+
+    // Send the notification to all clients on the current game except the currSession
+    public void broadcastMessage(Session currSession, ServerMessage message) throws IOException {
+        broadcastMessage(currSession, message, false);
+    }
+
+    // Send the notification to all clients on the current game
+    public void broadcastMessage(Session currSession, ServerMessage message, boolean toSelf) throws IOException {
+        System.out.printf("Broadcasting (toSelf: %s): %s%n", toSelf, new Gson().toJson(message));
+        for (Session session : Server.gameSessions.keySet()) {
+            boolean inAGame = Server.gameSessions.get(session) != 0;
+            boolean sameGame = Server.gameSessions.get(session).equals(Server.gameSessions.get(currSession));
+            boolean isSelf = session == currSession;
+            if ((toSelf || !isSelf) && inAGame && sameGame) {
+                session.getRemote().sendString(new Gson().toJson(message));
+            }
+        }
+    }
+
+    private void sendError(Session session, Error error) throws IOException {
+        session.getRemote().sendString(new Gson().toJson(error));
+    }
+
+    private ChessGame.TeamColor getTeamColor(String username, GameData game) {
+        if (username.equals(game.whiteUsername())) {
+            return ChessGame.TeamColor.WHITE;
+        }
+        else if (username.equals(game.blackUsername())) {
+            return ChessGame.TeamColor.BLACK;
+        }
+        else return null;
+    }
+
 }
